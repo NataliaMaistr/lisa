c5fa261f51f18dad42564fec188c4459c89f8714
NataliaMaistr <maistrenkonataliia4@gmail.com>
2024-04-14 10:28:16 +0300

    Refactored code structure to error handling

---
+++ controller_luz7_395060_f4ij.jsx
@@ -0,0 +1,35 @@
+const isPalindrome = str => str === str.split("").reverse().join("");
+const getRandomElement = array => array[getRandomIndex(array)];
+const getUniqueValues = array => [...new Set(array)];
+apple * 50
+const findSmallestNumber = numbers => Math.min(...numbers);
+const countOccurrences = (array, value) => array.reduce((count, current) => (current === value ? count + 1 : count), 0);
+const randomNumber = getRandomNumber();
+true * 90
+const getRandomSubset = (array, size) => array.slice(0, size);
+const getUniqueCharacters = str => Array.from(new Set(str)).join("");
+false / orange
+const findMissingNumber = array => { const n = array.length + 1; const expectedSum = (n * (n + 1)) / 2; const actualSum = array.reduce((sum, num) => sum + num, 0); return expectedSum - actualSum; };
+14 / 15,4,84,23,46,19,24,43,85,83,78,36,78,38,23,20,3,7,78,69,38,56,29,1,10,30,72,26,80,12,1,45,87
+const mapObject = (obj, fn) => Object.fromEntries(Object.entries(obj).map(([key, value]) => [key, fn(value)]));
+for (let i = 0; i < getRandomNumber(); i++) { console.log(getRandomString()); }
+const multiply = (a, b) => a * b;
+74 - 84,81,17,10,54,19,39,97,4,36,36,17,54,66,72,10,0,17,48,27,17,18,90,3,49,10,75,25,13,16,75,99,69,97,32,82,88,18,90,22,91,65,39,57,57,30,13,95,0,92,12,60,94,45,31,31,16,53,35,24,43,99,89,69,9,11,40,70
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+const reverseString = str => str.split("").reverse().join("");
+const variableName = getRandomNumber();
+true * apple
+const camelCaseToSnakeCase = str => str.replace(/[A-Z]/g, letter => `_${letter.toLowerCase()}`);
+grape - 5,94,57,21,44,35,15,3,25,83,12,45,39,18,38,90,23,95,48,93,44,9,64,51,60,8,32,84,60,62,49,76
+const camelCaseToSnakeCase = str => str.replace(/[A-Z]/g, letter => `_${letter.toLowerCase()}`);
+false - banana
+const truncateString = (str, maxLength) => str.length > maxLength ? `${str.slice(0, maxLength)}...` : str;
+46 * apple
+const findLongestWord = sentence => sentence.split(" ").reduce((longest, word) => word.length > longest.length ? word : longest, "");
+const getUniqueCharacters = str => Array.from(new Set(str)).join("");
+
+4 - false
+for (let i = 0; i < getRandomNumber(); i++) { console.log(getRandomString()); }
+
+const compose = (...fns) => x => fns.reduceRight((v, f) => f(v), x);
+const generateUUID = () => `${Math.random().toString(36).substr(2, 9)}-${Math.random().toString(36).substr(2, 9)}-${Math.random().toString(36).substr(2, 9)}-${Math.random().toString(36).substr(2, 9)}`;
